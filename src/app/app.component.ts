5c3595849aa29b4f4318b39d6d6373f634bf845d
Anselmo <ansinftec17@gmail.com>
2024-02-13 11:46:23 -0300

    FEAT:Adicionando estilização nos cards da pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pizzas } from './models/pizzas.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'La-pizza';
+
+
+  public pizzas: Array<Pizzas> = [
+    {
+      image: '../assets/pizza.png',
+      title: 'La Cuma',
+      content: 'Lorem ipsum dolor sit amet,consectetur adipiscing elit.',
+      price: 29.90,
+    },
+    {
+      image: '../assets/pizza.png',
+      title: 'La Menta',
+      content: 'Lorem ipsum dolor sit amet,consectetur adipiscing elit.',
+      price: 25.90,
+    },
+    {
+      image: '../assets/pizza.png',
+      title: 'La Toca',
+      content: 'Lorem ipsum dolor sit amet,consectetur adipiscing elit.',
+      price: 29.90,
+    },
+    {
+      image: '../assets/pizza.png',
+      title: 'La Top',
+      content: 'Lorem ipsum dolor sit amet,consectetur adipiscing elit.',
+      price: 25.50,
+     },
+    // {
+    //   image: '../assets/pizza.png',
+    //   title: 'La Menta',
+    //   content: 'Lorem ipsum dolor sit amet,consectetur adipiscing elit.',
+    //   price: 26.00,
+    // },
+    // {
+    //   image: '../assets/pizza.png',
+    //   title: 'La Toca',
+    //   content: 'Lorem ipsum dolor sit amet,consectetur adipiscing elit.',
+    //   price: 23.80,
+    // }
+  ]
 }
